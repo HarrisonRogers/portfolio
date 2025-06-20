2f752dd30e000ba41a3ce562ddc05544ba1f48ec
harrison rogers <harrison.rogers05@gmail.com>
2025-06-21 11:04:42 +1200

    change title to xl

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -24,7 +24,7 @@ function ProjectsPage() {
 
     return (
       <div className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4 text-black dark:text-white">
+        <h2 className="text-xl font-semibold mb-4 text-black dark:text-white">
           {sectionTitle}
         </h2>
         <div>
